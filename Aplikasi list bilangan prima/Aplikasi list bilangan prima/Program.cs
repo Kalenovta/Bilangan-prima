3ea42906aacdbe88df7cb5ee3b5dcd7970bf4544
Kalenovta <Kalenovtakhoirulanwar@gmail.com>
2024-08-16 13:12:10 +0700

    Add files via upload

---
+++ Aplikasi list bilangan prima/Aplikasi list bilangan prima/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikasi ");
+            Console.WriteLine("List Bilangan Prima");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Nama: Kalenovta Khoirul Anwar");
+            Console.WriteLine("No.Absen: 12");
+            Console.WriteLine("Kelas: X PPLG");
+            for (int num = 2; num <= 100; num++)
+            {
+                bool isPrime = true;
+
+                // Cek apakah bilangan prima atau bukan
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(num + " ");
+                }
+
+            }
+
+        }
+    }
